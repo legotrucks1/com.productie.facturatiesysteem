81d81b67697644c3e2b2a42013b5dbc72f023241
legotrucks1 <bashirasad1@gmail.com>
2021-09-16 14:28:34 +0200

    Alle producten aangemaakt in een map

--- src/ProductenDB.java
+++ src/ProductenDB.java
@@ -12,10 +12,18 @@ public class ProductenDB extends TreeMap {
             new ArrayList<>(Arrays.asList("Drank", "Frisdrank")),
             new ArrayList<>(Arrays.asList("URL1", "URL2", "URL3")),
             new ArrayList<>(Arrays.asList("250ML", "330ml", "500ML", "1L", "1,5L", "2L")));
-    Product p3 = new Product("Coca-Cola",
-            new ArrayList<>(Arrays.asList("Drank", "Frisdrank")),
-            new ArrayList<>(Arrays.asList("URL1", "URL2", "URL3")),
-            new ArrayList<>(Arrays.asList("250ML", "330ml", "500ML", "1L", "1,5L", "2L")));
+    Product p3 = new Product("Shampoo",
+            new ArrayList<>(Arrays.asList("Verzorgingsproducten")),
+            new ArrayList<>(Arrays.asList("URL1", "URL2")),
+            new ArrayList<>(Arrays.asList("400ML", "500ML", "600ML")));
+    Product p4 = new Product("Kinder-Bueno",
+            new ArrayList<>(Arrays.asList("Snoep")),
+            new ArrayList<>(Arrays.asList("URL1")),
+            new ArrayList<>(Arrays.asList("150GR", "500GR")));
+    Product p5 = new Product("Dreft",
+            new ArrayList<>(Arrays.asList("Wasmiddel")),
+            new ArrayList<>(Arrays.asList("URL1", "URL2")),
+            new ArrayList<>(Arrays.asList("400ML", "500ML", "600ML", "800ML")));
     //Product p1 = new Product();
 
 
